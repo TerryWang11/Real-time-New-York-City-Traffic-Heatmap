833c043ccbdc689045e42ad83f935fef50711573
Zhenrui Chen <chenzhenrui@dyn-160-39-182-139.dyn.columbia.edu>
2022-05-08 22:10:31 -0400

    add weather and playback function route

--- google maps/server.py
+++ google maps/server.py
@@ -1,16 +1,28 @@
-import os
+from asyncore import read
+import csv
+import imp
 import re
+import json
+from select import select
+from tracemalloc import start
 from flask import Flask
 from flask import render_template
-from flask import request, Response
+from flask import request, Response, jsonify
 from flask import g, redirect
+import sys
+sys.path.append("../")
+import tools.fuc
+
 
 app = Flask(__name__)
 
 import pymysql
-conn = pymysql.connect(host='localhost', user='vulclone', password='1234', database="ELEN6889", charset="utf8")
+conn = pymysql.connect(host='localhost', user='root', password='dbuserdbuser', database="ELEN6889", charset="utf8")
 
 mycursor = conn.cursor()
+end_time = "2022_5_5_12_0"
+weatherList = {"Clear": "1", "Clouds": "2", "Tornado": "3", "Rain": "4", "Fog": "5"}
+
 
 # @app.before_request
 # def before_request():
@@ -37,26 +49,154 @@ mycursor = conn.cursor()
 #     except Exception as e:
 #         pass
 
-@app.route('/')
-def home():
-    """
-    This function is used to process the data from database to frontend part in the formation of string array
-    """
-    q1 = '''SELECT points,rating FROM `2022_5_1_22_59_21`'''
-    mycursor.execute(q1)
+def read_csv(route):
+    csv_reader = csv.reader(open(route))
+    points = []
+    for line in csv_reader:
+        latlng = []
+        latlng.append(float(line[0]))
+        latlng.append(float(line[1]))
+        date = line[2][:10]
+        res = [latlng, date]
+        points.append(res)
+    # print(points)
+    return points
+
+def process_mysql(q):
+    mycursor.execute(q)
     data = mycursor.fetchall()
-    # print(data[0][1])
-    # print(len(data))
     dataLen = len(data)
     result = []
     rating = []
     for i in range(dataLen):
         line = data[i][0].replace(" ", "")[2:-2].split('},{')
         result.append(line)
-        rating.append(int(data[i][1]))
-    # print(result)
-    # print(type(rating[0]))
-    return render_template("snapToRoad.html", result=result, len=dataLen, rating=rating)
+        rating.append(float(data[i][1]))
+    
+    # weather = weatherList[data[0][2]]
+    weather = str(weatherList[data[0][2]])
+    print(weather)
+    return result, rating, weather, dataLen
+
+@app.route('/playBack', methods=['POST'])
+def playBack():
+    # start_time = "2022_05_05_12_00"
+    start_time = request.get_json()
+    conStrs = "T-:"
+    for conStr in conStrs:
+        start_time = start_time.replace(conStr, "_")
+
+    q = "SELECT points, rating, weather from `%s`"%(start_time)
+    result, rating, weather, dataLen = process_mysql(q)
+    data = []
+    data.append(result)
+    data.append(rating)
+    data.append(weather)
+    data.append(dataLen)
+    
+    # get the next date
+    min_index = start_time.rfind("_")
+    interval = 15 # We call the API every 15 min
+    min = int(start_time[min_index + 1:])
+    rest = start_time[:min_index]
+    min = min + interval
+
+    if (min == 60):
+        min = 0
+        hour_index = rest.rfind("_")
+        hour = int(rest[hour_index + 1:])
+        hour = hour + 1
+        rest = rest[:hour_index]
+        rest = rest + "_" + tools.fuc.pro_name(str(hour))
+
+    start_time = rest + "_" + tools.fuc.pro_name(str(min))
+    print(start_time)
+    if (start_time == end_time):
+        info = "Finish"
+        return  json.dumps(info)
+
+    start_time = start_time.replace("_", "-");
+    start_time = start_time[:13] + ":" + start_time[14:]
+    start_time = start_time[:10] + "T" + start_time[11:]
+    print(start_time)
+
+    data.append(start_time)
+    return json.dumps(data)
+
+
+@app.route('/get_history_time', methods=['POST'])
+def get_history_time():
+    global history
+    history = request.get_json()
+    conStrs = "T-:"
+    for conStr in conStrs:
+        history = history.replace(conStr, "_")
+    print(history)
+    # history = "2022_05_05_09_15"
+    q = "SELECT points, rating, weather FROM `%s`"%(history)
+    result, rating, weather, dataLen = process_mysql(q)
+
+    data = []
+    data.append(result)
+    data.append(rating)
+    data.append(weather)
+    data.append(dataLen)
+
+    return json.dumps(data)
+
+@app.route('/get_history_weather', methods=['POST'])
+def get_weather():
+    weather = request.get_json()
+    data = []
+
+    result = []
+    rating = []
+    dataLen = 0
+    history_time = ""
+    if (weather == "2"):
+        q = "SELECT points, rating, weather FROM `2022_05_05_19_00`"
+        result, rating, weather, dataLen = process_mysql(q)
+        history_time = "2022_05_05_20_00"
+
+    elif (weather == "1"):
+        q = "SELECT points, rating, weather FROM `2022_05_05_20_30`"
+        result, rating, weather, dataLen = process_mysql(q)
+        history_time = "2022_05_05_20_30"
+    
+    elif (weather == "4"):
+        q = "SELECT points, rating, weather FROM `2022_05_05_12_30`"
+        result, rating, weather, dataLen = process_mysql(q)
+        history_time = "2022_05_05_12_30"
+
+    else:
+        return jsonify(data = "None")
+
+    if history_time:
+        history_time = history_time.replace("_", "-");
+        history_time = history_time[:13] + ":" + history_time[14:]
+        history_time = history_time[:10] + "T" + history_time[11:]
+
+    print(history_time)
+
+    data.append(result)
+    data.append(rating)
+    data.append(weather)
+    data.append(dataLen)
+    data.append(history_time)
+
+    return json.dumps(data)
+
+@app.route('/')
+def home():
+    """
+    This function is used to process the data from database to frontend part in the formation of string array
+    """
+    q = "SELECT points, rating, weather FROM `2022_05_05_20_30`"
+    result, rating, weather, dataLen = process_mysql(q)
+
+    route = "../crash_data.csv"
+    points = read_csv(route)
+    return render_template("snapToRoad.html", result=result, len=dataLen, rating=rating, weather=weather,points=points)
 
 
 if __name__ == '__main__':
@@ -88,5 +228,4 @@ if __name__ == '__main__':
     # app.run(debug=True)
 
 # if __name__ == "__main__":
-#     home(mycursor)
-
+#     playBack()
